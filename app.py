8d1d8a9507be3b32913aa825328d977ebe32505d
Maria Clara Mota <mariaclaramota205@gmail.com>
2026-07-01 18:48:39 -0300

    Update Google Sheet and Drive folder IDs

---
+++ app.py
@@ -0,0 +1,2 @@
+ID_PLANILHA_GOOGLE = https://docs.google.com/spreadsheets/d/1JrUGFV8cwRR7niP3y95UMg8Q5nbj9adGjrkvnDzJon4/edit?usp=sharing
+ID_PASTA_FOTOS_DRIVE = https://drive.google.com/drive/folders/1DQwCGlv7DM-WOmw__WkpwbxVc7VhtcvK?usp=drive_link
